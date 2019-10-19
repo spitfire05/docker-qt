7bf42c59299a47ffa14b629bb070666be79416f6
Aurélien Brooke <dev@abrooke.fr>
2019-10-19 14:50:37 +0200

    Remove the useless QtCharts

--- scripts/install-qt.sh
+++ scripts/install-qt.sh
@@ -20,7 +20,7 @@ set -e #quit on error
 
 mkdir /tmp/qt
 curl -Lo /tmp/qt/installer.run "https://download.qt.io/archive/qt/$(echo "${QT_VERSION}" | cut -d. -f 1-2)/${QT_VERSION}/qt-opensource-linux-x64-${QT_VERSION}.run"
-QT_CI_PACKAGES=qt.qt5.$(echo "${QT_VERSION}" | tr -d .).${QT_PLATFORM},qt.qt5.$(echo "${QT_VERSION}" | tr -d .).qtcharts \
+QT_CI_PACKAGES=qt.qt5.$(echo "${QT_VERSION}" | tr -d .).${QT_PLATFORM} \
 /tmp/build/bin/extract-qt-installer /tmp/qt/installer.run "$QT_PATH"
 find "${QT_PATH}" -mindepth 1 -maxdepth 1 ! -name "${QT_VERSION}" -exec echo 'Cleaning Qt SDK: {}' \; -exec rm -r '{}' \;
 rm -rf /tmp/qt
