e6a86c378b07d9108a6f5e145f61d291ce28fc29
Aurélien Brooke <dev@abrooke.fr>
2019-11-08 10:46:26 +0100

    Create a REQUIRED_SDK_PLATFORM variable for easier update

--- scripts/install-android-sdk.sh
+++ scripts/install-android-sdk.sh
@@ -18,6 +18,10 @@ if [ -z "$SDK_PACKAGES" ]; then
     exit 1
 fi
 
+# Uploading an app to Google Play requires a particular target API level
+# https://support.google.com/googleplay/android-developer/answer/113469#targetsdk
+REQUIRED_SDK_PLATFORM=android-28
+
 set -e #quit on error
 
 curl -Lo /tmp/sdk-tools.zip 'https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip'
@@ -25,4 +29,4 @@ unzip -q /tmp/sdk-tools.zip -d ${ANDROID_SDK_ROOT}
 rm -fv /tmp/sdk-tools.zip
 yes | sdkmanager --licenses
 sdkmanager --update
-sdkmanager --verbose "platforms;${SDK_PLATFORM}" "platforms;android-28" "build-tools;${SDK_BUILD_TOOLS}" ${SDK_PACKAGES}
+sdkmanager --verbose "platforms;${SDK_PLATFORM}" "platforms;${REQUIRED_SDK_PLATFORM}" "build-tools;${SDK_BUILD_TOOLS}" ${SDK_PACKAGES}
